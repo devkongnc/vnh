4bfec189018b90758c6eaf665aa15bf8d2615afc
Kong Nguyen <dev.kongnc@gmail.com>
2018-07-06 18:24:03 +0700

    vnh-kong update sendmail

--- resources/views/partials/contact_form.blade.php
+++ resources/views/partials/contact_form.blade.php
@@ -1,41 +1,53 @@
-<form action="{{ action('HomeController@contact') }}" method="POST" class="form-contact" data-prefix="{{ $prefix }}">
+<form action="{{ action('HomeController@contact') }}" method="POST" class="contact-form" data-prefix="{{ $prefix }}">
     {{ csrf_field() }}
+    <hr>
     <div class="estate-ids hidden">
         @if (isset($estate_id))
-        <input type="hidden" name="estates[]" value="{{ $estate_id }}">
+            <input type="hidden" name="estates[]" value="{{ $estate_id }}">
         @endif
     </div>
-    <div class="title">
-        @lang('entity.page.contact.description') <span class="icon-require"></span> @lang('entity.page.contact.required')
-    </div>
     <div class="contact-errors"></div>
-    <div class="wrap-form">
-        <div class="group-form">
-            <label for="{{ $prefix }}-name">@lang('entity.page.contact.name') <span class="icon-require"></span></label>
-            <input type="text" id="{{ $prefix }}-name" name="name" value="" required="" maxlength="255" title="{{ rtrim(trans('validation.min.string', ['attribute' => 'name', 'min' => 3]), '.') }}" />
-        </div>
-        <div class="group-form">
-            <label for="{{ $prefix }}-phone">@lang('entity.page.contact.phone') <span class="icon-require"></span></label>
-            <input type="tel" id="{{ $prefix }}-phone" name="phone" value="" required="" minlength="8" maxlength="15" pattern="^\+?[0-9\s]+$" title="{{ rtrim(trans('validation.regex', ['attribute' => 'phone']), '.') }}" />
-        </div>
-        <div class="group-form">
-            <label for="{{ $prefix }}-email">@lang('entity.page.contact.email') <span class="icon-require"></span></label>
-            <input type="email" id="{{ $prefix }}-email" name="email" value="" required="" maxlength="255" /> <span>（@lang('entity.page.contact.alphanumeric')）</span>
-        </div>
-        <div class="group-form">
-            <label for="{{ $prefix }}-message" style="margin-top: 5px;">@lang('entity.page.contact.message') <span class="icon-require"></span></label>
-            <textarea id="{{ $prefix }}-message" name="message" required="" maxlength="500"></textarea>
+    <div class="gr-cotnact-row">
+        <label>Name <img src="/images/new-layout/icon-require.png" ></label>
+        <input type="text" id="{{ $prefix }}-name" name="name" value="" required="" maxlength="255" title="{{ rtrim(trans('validation.min.string', ['attribute' => 'name', 'min' => 3]), '.') }}" />
+    </div>
+    <div class="gr-cotnact-row">
+        <label>Your phone number <img src="/images/new-layout/icon-require.png" ></label>
+        <input type="tel" id="{{ $prefix }}-phone" name="phone" value="" required="" minlength="8" maxlength="15" pattern="^\+?[0-9\s]+$" title="{{ rtrim(trans('validation.regex', ['attribute' => 'phone']), '.') }}" />
+    </div>
+    <div class="gr-cotnact-row">
+        <label>E-mail address <img src="/images/new-layout/icon-require.png" ></label>
+        <input type="email" id="{{ $prefix }}-email" name="email" value="" required="" maxlength="255" /> <span>（@lang('entity.page.contact.alphanumeric')）</span>
+    </div>
+    <div class="gr-cotnact-row">
+        <label>Content of inquiry <img src="/images/new-layout/icon-require.png" ></label>
+        <textarea id="{{ $prefix }}-message" name="message" required="" maxlength="500"></textarea>
+    </div>
+    <div class="gr-cotnact-row">
+        @if ($prefix === 'contact') <div id="RecaptchaContact" class="g-recaptcha"></div>
+        @elseif ($prefix === 'estate') <div id="RecaptchaEstate" class="g-recaptcha"></div>
+        @else <div id="RecaptchaPopup" class="g-recaptcha"></div>
+        @endif
+    </div>
+    <hr>
+    <div class="gr-cotnact-row">
+        <div class="submit-blk">
+            <span class="yellow-txt">Privacy policy </span>Please read through to the confirmation screen.
+            <button type="submit" class="btn-send blk-submit-btn">@lang('entity.page.contact.submit')</button><img src="{{ asset('images/ajax-loader.gif') }}" class="ajax-loader hidden" alt="ajax loader">
         </div>
     </div>
-    <div class="policy">
-        <p><a href="{{ LaravelLocalization::getLocalizedURL(null, $pages_by_id[5]->permalink) }}" target="_blank">@lang('entity.page.contact.policy')</a>@lang('entity.page.contact.policy description')</p>
-        <!-- <p>
-            <input id="{{ $prefix }}-checkpolicy" name="policy" type="checkbox" required="" />
-            <label for="{{ $prefix }}-checkpolicy">@lang('entity.page.contact.policy checkbox')</label>
-        </p> -->
-    </div>
-    @if ($prefix === 'contact') <div id="RecaptchaContact" class="g-recaptcha"></div>
-    @elseif ($prefix === 'estate') <div id="RecaptchaEstate" class="g-recaptcha"></div>
-    @else <div id="RecaptchaPopup" class="g-recaptcha"></div> @endif
-    <button type="submit" class="btn-send">@lang('entity.page.contact.submit')</button><img src="{{ asset('images/ajax-loader.gif') }}" class="ajax-loader hidden" alt="ajax loader">
 </form>
+
+<style>
+    .gr-cotnact-row input{
+        height: 36px;
+        line-height: 36px;
+        padding: 6px 10px;
+        min-width: 340px;
+    }
+    .gr-cotnact-row input:hover{
+        border: 1px solid #fbb03b;
+        outline-color: transparent;
+        outline-style: none;
+    }
+</style>
