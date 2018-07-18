e955bcfa69e9cea4e3f564251334d0a47421b139
Your Name <you@example.com>
2018-07-18 19:03:22 +0700

    upadte print

--- resources/views/estate/show.blade.php
+++ resources/views/estate/show.blade.php
@@ -251,3 +251,19 @@
 
     </script>
 @endsection
+
+@section('styles')
+    <style type="text/css" media="print">
+        @media print {
+            * {
+                -webkit-print-color-adjust: exact; !important;
+                color-adjust: exact !important;
+            }
+        }
+        @page {
+            size:A4 landscape;
+            margin: 10px;
+            -webkit-print-color-adjust: exact;
+        }
+    </style>
+@endsection
