efe5a42ed48ae588f886190cb86c398465ce8279
Your Name <you@example.com>
2018-07-31 20:56:28 +0700

    upload contact

--- resources/views/partials/contact_form.blade.php
+++ resources/views/partials/contact_form.blade.php
@@ -1,4 +1,4 @@
-<form action="{{ action('HomeController@contact') }}" method="POST" class="contact-form" data-prefix="{{ $prefix }}">
+<form id="form" action="{{ action('HomeController@contact') }}" method="POST" class="contact-form" data-prefix="{{ $prefix }}">
     {{ csrf_field() }}
     <hr>
     <div class="estate-ids hidden">
@@ -17,67 +17,113 @@
     <div class="gr-cotnact-row">
         <label class="ctf_hide_mobile">@lang('entity.page.contact.name') <img src="/images/new-layout/icon-require.png" ></label>
         <span class="ctf-show-mobile"><img src="/images/icon-contact-form/user-icon.png" ></span>
-        <span class="ctf-validation-field">
+        <div class="ctf-validation-field">
             <input type="text" id="{{ $prefix }}-name" name="name" value="" placeholder="@lang('entity.page.contact.name')" required="" maxlength="255" title="{{ rtrim(trans('validation.min.string', ['attribute' => 'name', 'min' => 3]), '.') }}" />
-        </span>
+        </div>
     </div>
     <div class="gr-cotnact-row">
         <label class="ctf_hide_mobile">@lang('entity.page.contact.phone') <img src="/images/new-layout/icon-require.png" ></label>
         <span class="ctf-show-mobile"><img src="/images/icon-contact-form/phone-icon.png" ></span>
-        <span class="ctf-validation-field">
+        <div class="ctf-validation-field">
             <input type="tel" id="{{ $prefix }}-phone" name="phone" value="" placeholder="@lang('entity.page.contact.phone')" required="" minlength="8" maxlength="15" pattern="^\+?[0-9\s]+$" title="{{ rtrim(trans('validation.regex', ['attribute' => 'phone']), '.') }}" />
-        </span>
+        </div>
         <span class="ctf_hide_mobile">（@lang('entity.page.contact.alphanumeric')）</span>
     </div>
     <div class="gr-cotnact-row">
         <label class="ctf_hide_mobile">@lang('entity.page.contact.email') <img src="/images/new-layout/icon-require.png" ></label>
         <span class="ctf-show-mobile"><img src="/images/icon-contact-form/email-icon.png" ></span>
-        <span class="ctf-validation-field">
+        <div class="ctf-validation-field">
             <input type="email" id="{{ $prefix }}-email" name="email" value="" placeholder="@lang('entity.page.contact.email')" required="" maxlength="255" />
-        </span>
+        </div>
     </div>
     <div class="gr-cotnact-row">
         <label class="ctf_hide_mobile">@lang('entity.page.contact.message') <img src="/images/new-layout/icon-require.png" ></label>
         <span class="ctf-show-mobile"><img src="/images/icon-contact-form/mess-icon.png" ></span>
-        <span class="ctf-validation-field">
+        <div class="ctf-validation-field">
             <textarea id="{{ $prefix }}-message" name="message" required="" maxlength="500" placeholder="@lang('entity.page.contact.message')"></textarea>
-        </span>
+        </div>
     </div>
     <div class="gr-cotnact-row">
-        @if ($prefix === 'contact') <div id="RecaptchaContact" class="g-recaptcha"></div>
-        @elseif ($prefix === 'estate') <div id="RecaptchaEstate" class="g-recaptcha"></div>
-        @else <div id="RecaptchaPopup" class="g-recaptcha"></div>
-        @endif
+        {{--@if ($prefix === 'contact') <div id="RecaptchaContact" class="g-recaptcha"></div>--}}
+        {{--@elseif ($prefix === 'estate') <div id="RecaptchaEstate" class="g-recaptcha"></div>--}}
+        {{--@else <div id="RecaptchaPopup" class="g-recaptcha"></div>--}}
+        {{--@endif--}}
+        <div class="g-recaptcha" data-sitekey="6LfdbmcUAAAAAGPmOrdaAFMt6mTQeBn7UsNkE0y_" data-callback="correctCaptcha"></div>
+        <div id="recaptcha-error" style="display: none; margin-top:5px; color: #C22F12">Please complete the recaptcha</div>
     </div>
     <hr>
     <div class="gr-cotnact-row">
         <div class="submit-blk">
             <span class="yellow-txt">@lang('entity.page.contact.policy') </span>@lang('entity.page.contact.policy description')
-            <button type="submit" class="btn-send blk-submit-btn">@lang('entity.page.contact.submit')</button><img src="{{ asset('images/ajax-loader.gif') }}" class="ajax-loader hidden" alt="ajax loader">
+            <button type="button" class="btn-send blk-submit-btn">@lang('entity.page.contact.submit')</button><img src="{{ asset('images/ajax-loader.gif') }}" class="ajax-loader hidden" alt="ajax loader">
         </div>
     </div>
 </form>
 
+<script src='https://www.google.com/recaptcha/api.js'></script>
 
+<script>
+    $('.blk-submit-btn').click(function (e) {
+        if($('.gr-cotnact-row input, .gr-cotnact-row textarea').val() != ''){
+            var response = grecaptcha.getResponse();
+            //recaptcha failed validation
+            if(response.length == 0) {
+                e.preventDefault();
+                $('#recaptcha-error').show();
+                console.log('fail');
+            }
+            //recaptcha passed validation
+            else {
+                $('#recaptcha-error').hide();
+                $( "#form" ).submit();
+                console.log('pass');
+            }
+        }else{
+            $('.gr-cotnact-row input, .gr-cotnact-row textarea').addClass('field-empty');
+            return false;
+        }
+    });
+    $('.gr-cotnact-row input, .gr-cotnact-row textarea').bind('keyup blur change click', function () {
+        // console.log('null');
+        if ($(this).val() == ''){
+            $(this).addClass('field-empty');
+            $(this).parent().removeClass('input-success');
+            $('.blk-submit-btn').addClass('btn-disable');
+            $('.blk-submit-btn').attr('disabled', true);
+        }else{
+            $(this).removeClass('field-empty');
+            $(this).parent().addClass('input-success');
+            $('.blk-submit-btn').removeClass('btn-disable');
+            $('.blk-submit-btn').removeAttr('disabled');
+        }
+    });
+</script>
 
 <style type="text/css">
-    .gr-cotnact-row input{
+    .ctf-validation-field{
+        width: 340px;
+        display: inline-block;
+    }
+    .input-success{position: relative;}
+    .input-success:after{
+        content: "\f00c";
+        font-family: FontAwesome;
+        position: absolute;
+        right: 0;
+        top: 0;
+        color: #D6D6D6;
+        font-size: 18px;
+        height: 35px;
+        width: 35px;
+        line-height: 35px;
+        text-align: center;
+    }
+    .ctf-validation-field input{
         padding: 6px 10px;
         height: 40px;
         line-height: 40px;
         position: relative;
-    }
-    .gr-cotnact-row .ctf-validation-field:after{
-        content: "\e911";
-        font-family: 'icomoon' !important;
-        position: absolute;
-        right: 0;
-        top: 0;
-        width: 45px;
-        height: 45px;
-        line-height: 45px;
-        text-align: center;
-        color: #D6D6D6;
+        width: 100%;
     }
 
     .gr-cotnact-row input:hover{
@@ -91,7 +137,7 @@
     @media screen and (max-width: 767px){
         .ctf_hide_mobile{display: none !important}
         .ctf-show-mobile{display: block; float: left; padding-top: 5px;}
-        .gr-cotnact-row input, .gr-cotnact-row textarea{margin-left: 5%; width: 83% !important;}
+        .ctf-validation-field{margin-left: 5%; width: 83% !important;}
     }
 
     @media screen and (min-width: 768px){
@@ -104,6 +150,15 @@
         }
         .gr-cotnact-row input{min-width: 340px;}
     }
+
+    .gr-cotnact-row .field-empty{
+        border: 2px solid #C22F12 !important;
+    }
+    .btn-disable, .btn-disable:hover{
+        background: #ccc;
+        cursor: default;
+    }
 </style>
+
 
 
